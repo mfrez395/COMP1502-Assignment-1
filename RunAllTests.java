fa85784ce3b57c6ede6b7cf00c54480cb59082c4
Mark Frezell <mfrez395@mtroyal.ca>
2019-02-08 15:49:17 -0700

    Added file RunAllTests.java

---
+++ RunAllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestPlayer.class, TestPlayerList.class })
+public class AllTests {
+
+}
